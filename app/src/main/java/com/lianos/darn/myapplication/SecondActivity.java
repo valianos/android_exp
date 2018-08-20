4c30332eb95d90b0f1183e2d0eb0a4e269209e87
Vasilis Lianos <vlianos96@gmail.com>
2018-08-21 00:27:16 +0300

    Implement second activity and exit the app.

--- app/src/main/java/com/lianos/darn/myapplication/SecondActivity.java
+++ app/src/main/java/com/lianos/darn/myapplication/SecondActivity.java
@@ -1,13 +1,12 @@
 package com.lianos.darn.myapplication;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,25 +14,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         // Link with activity xml file.
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
 
-        ImageButton button = findViewById(R.id.button);
+        ImageButton button = findViewById(R.id.little_guy);
 
         // Bind button with listener.
         button.setOnClickListener(new ClickListener());
 
     }
 
-
     public class ClickListener implements View.OnClickListener {
 
         @Override
         public void onClick(View v) {
 
-            Log.d("Debug message", "Clicked little fella");
-
-            Intent second = new Intent(MainActivity.this, SecondActivity.class);
-            MainActivity.this.startActivity(second);
+            Log.d("Debug message", "Clicked to exit");
 
             finish();
 
